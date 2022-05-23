248f98abd8b17b0c6e9ae001ffbde62e0bd00917
fahrazi12 <ivallavif@gmail.com>
2022-05-23 19:10:26 +0700

    push project

--- src/main/java/com/investree/demo/repository/TransaksiRepository.java
+++ src/main/java/com/investree/demo/repository/TransaksiRepository.java
@@ -1,13 +1,14 @@
 package com.investree.demo.repository;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import com.investree.demo.model.Transaksi;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Slice;
 
-import java.awt.print.Pageable;
 
 @Repository
 public interface TransaksiRepository extends PagingAndSortingRepository<Transaksi, Long> {
